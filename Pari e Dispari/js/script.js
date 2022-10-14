828f48333cc8df7ac83b309c7644d953d0a0df3d
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-14 17:54:52 +0200

    add random(1/5)function

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -2,10 +2,21 @@
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 const userNumber = parseInt(prompt("inserisci un numero da 1 a 5"));
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-const cpuNumber = random();
+const cpuNumber = randomNumber1to5();
+console.log(cpuNumber);
 // Sommiamo i due numeri
 const sum = cpuNumber + userNumber;
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 OddEven(sum);
 // Dichiariamo chi ha vinto.
 alert(msg);
+
+
+
+
+
+// FUNCTIONS
+function randomNumber1to5 () {
+    const randomNumber = Math.floor(Math.random() * 5) + 1;
+    return randomNumber;
+}
