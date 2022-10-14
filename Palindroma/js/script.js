c3ce0fe1ed4f3eb3c8afb044ef00209a9a965217
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-14 16:37:27 +0200

    add function

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,20 +1,52 @@
 // **Palindroma**
 // Chiedere all’utente di inserire una parola
-const word = prompt("inserisci una parola");
+const input = palindrome(prompt("inserisci una parola"));
+
+console.log("TXT: ", input);
+
+alert(input);
+
+
+
+
+
+
+
+
+
 // Creare una funzione per capire se la parola inserita è palindroma.
-const array = word.split('');
-console.log(array);
+/**
+ * Description
+ * @param {string} word è la parola da inserire per la verifica del palindromo
+ * @returns {string} msg: è il msg output
+ */
 
-const arrayLength = array.length;
-let checkPalindrome = true;
-
-for (i = 0; i < (arrayLength); i++) {
-    let arrayLengthMinus = ((arrayLength - 1) - i);
-    if (array[i] != array[arrayLengthMinus]) {
-        checkPalindrome = false;
-        console.log("controllo checkPalindrome", checkPalindrome);
+function palindrome(word) {
+    
+    const array = word.split('');
+    console.log(array);
+    
+    const arrayLength = array.length;
+    let checkPalindrome = true;
+    
+    for (i = 0; i < (arrayLength); i++) {
+        let arrayLengthMinus = ((arrayLength - 1) - i);
+        if (array[i] != array[arrayLengthMinus]) {
+            checkPalindrome = false;
+            console.log("controllo checkPalindrome", checkPalindrome);
+        }
     }
-}
+    
+    console.log("checkPalindrome è", checkPalindrome);
 
-console.log("checkPalindrome è", checkPalindrome);
+    let msg;
+
+    if (checkPalindrome = true) {
+        msg = "non ci vuole un genio per capire che la tua parola è un palindromo";
+    } else {
+        msg = "idiota nn vedi che ci sono lettere diverse";
+    }
+
+    return msg;
+}
 
