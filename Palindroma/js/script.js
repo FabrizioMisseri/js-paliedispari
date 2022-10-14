f72381bae1b8655b895eec459751d775dff48866
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-14 16:17:25 +0200

    add checkPalindrome code

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,3 +1,20 @@
 // **Palindroma**
 // Chiedere all’utente di inserire una parola
+const word = prompt("inserisci una parola");
 // Creare una funzione per capire se la parola inserita è palindroma.
+const array = word.split('');
+console.log(array);
+
+const arrayLength = array.length;
+let checkPalindrome = true;
+
+for (i = 0; i < (arrayLength); i++) {
+    let arrayLengthMinus = ((arrayLength - 1) - i);
+    if (array[i] != array[arrayLengthMinus]) {
+        checkPalindrome = false;
+        console.log("controllo boolF", checkPalindrome);
+    }
+}
+
+console.log("boolFalse", checkPalindrome);
+
